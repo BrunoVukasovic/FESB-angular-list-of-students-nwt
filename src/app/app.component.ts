f4b0beec4bf464509d81498bc1d2c37664b8aad8
Bruno Vukasovic <vukasovic.bruno@gmail.com>
2018-12-23 18:05:32 +0100

    table created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Student {
+  id: number;
+  fullName: string;
+  birthdate: Date;
+  faculty: string;
+  marked?: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-lab';
+
+export class AppComponent implements OnInit {
+  // title = 'angular-lab';
+  title: string;
+  students: Student[];
+
+  constructor() {
+    this.title = 'List of Students';
+  }
+
+  ngOnInit() {
+    this.students = this.getStudents();
+  }
+
+  getStudents(): Student[] {
+    const studentList = [
+      {
+        id: 1,
+        fullName: 'Mate Matic',
+        birthdate: new Date (1995, 10, 25),
+        faculty: 'FESB'
+      },
+      {
+        id: 2,
+        fullName: 'Ivo Ivic',
+        birthdate: new Date (1995, 11, 15),
+        faculty: 'FER'
+      },
+      {
+        id: 1,
+        fullName: 'Ante Antic',
+        birthdate: new Date (1995, 11, 29),
+        faculty: 'FESB'
+      }
+    ];
+    return studentList;
+  }
+
+
 }
