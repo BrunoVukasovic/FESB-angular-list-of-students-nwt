bf461548988a60e5f6f9062bb0c0e0dcadebdd9f
Bruno Vukasovic <vukasovic.bruno@gmail.com>
2018-12-24 20:37:58 +0100

    student-deatils component added

--- src/app/components/student-details/student-details.component.ts
+++ src/app/components/student-details/student-details.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { InvokeFunctionExpr } from '@angular/compiler';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { IStudent } from 'src/app/models/student.model';
+import { ActivatedRoute } from '@angular/router';
+import { StudentService } from 'src/app/services/student.service';
 
 @Component({
   selector: 'app-student-details',
   templateUrl: './student-details.component.html',
   styleUrls: ['./student-details.component.css']
 })
-export class StudentDetailsComponent implements OnInit {
-  @Input() student: any;
-  @Input() index: number;
+export class StudentDetailsComponent implements OnInit, OnDestroy {
+  student: IStudent;
+  private subscription: any;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private studentService: StudentService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.subscription = this.route.params.subscribe(params => {
+      console.log(params);
+      this.studentService.getStudents().subscribe(
+        (students: IStudent[]) => {
+          console.log(students);
+          this.student = students.find(
+            (s: IStudent) => s.id === +params['id']);
+        });
+    });
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
